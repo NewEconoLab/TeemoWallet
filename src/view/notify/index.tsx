5fbacc23c9a249244cfa9286a9110870646871a3
dabanli123 <1031737848@qq.com>
2019-02-19 10:44:08 +0800

    修改提交文件配置

--- src/view/notify/index.tsx
+++ src/view/notify/index.tsx
@@ -2,12 +2,28 @@ import Notify from "./Notify";
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-chrome.tabs.query({ active: true, currentWindow: true }, tab => {
-    const name = "Teemmo Notify";
-    const app = document.createElement('div');
-    document.body.appendChild(app);
-    ReactDOM.render
-    (
-        <Notify name={name} />,app
-    );
-});
+if(chrome.tabs){
+    chrome.tabs.query({ active: true, currentWindow: true }, tab => {
+        const name = "Teemmo Notify";
+        const app = document.createElement('div');
+        document.body.appendChild(app);
+        ReactDOM.render
+        (
+            <Notify name={name} />,app
+        );
+    });
+}
+else{
+    window.onload=()=>
+    {   
+        //初始化鼠标随机方法
+        Neo.Cryptography.RandomNumberGenerator.startCollectors();
+        const app = document.createElement('div');
+        document.body.appendChild(app);
+        document.body.style.background="#b2b2b2";
+        app.style.background="#fff";
+        ReactDOM.render(
+            <Notify name={name}/>,app
+        );
+    }
+}
